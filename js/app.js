b9c54cf217e203eb8025d45d2ced64bdb23cb954
Frakto01200 <22110007@oic-ok.ac.jp>
2024-09-11 09:27:36 +0900

    完成まじか

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
+let deckId;
+let totalPlayer = 0;
+let totalCpu = 0;
+let sum = 0;
+let cpuCards = [];
 
-//画面読み込み時
-// window.onload = function onLoad() {}
-// console.log("onload");
+const deckApiUrl = "https://deckofcardsapi.com/api/deck";
 
-class Card {
-    constructor(type, num) {
-        this.type = type;
-        this.num = num;
-    }
-}
-//山札配列
-const cards = [];
-//山札から引いた数
-let picup_cnt = 0;
-let hit_flg = false;
-
-//手札配列
-const player_cards = [];
-let p_sum = 0;
-
-
-/// localStorageから目標値を取得して表示
+// localStorageから目標値を取得して表示
 const target = localStorage.getItem("target_value");
 if (target) {
     document.getElementById("target_text").textContent = target;
@@ -28,118 +14,190 @@ if (target) {
     document.getElementById("target_text").textContent = "目標値が設定されていません";
 }
 
+// リロードした場合に発生する機能
+window.onload = () => {
+    // ゲーム開始時にデッキを取得（山札）
+    fetch(`${deckApiUrl}/new/shuffle/?deck_count=6`)
+        .then(response => response.json())
+        .then(data => {
+            deckId = data.deck_id;
+            const fudaCardDiv = document.getElementById('fuda-cards');
+            const deckBackImg = document.createElement('img');
+            deckBackImg.src = "https://deckofcardsapi.com/static/img/back.png";
+            fudaCardDiv.appendChild(deckBackImg);
+        });
 
+    // playerとcpuの手札を2枚ずつ配る
+    fetch(`${deckApiUrl}/new/draw/?count=4`)
+        .then(response => response.json())
+        .then(data => {
+            const playerCardsDiv = document.getElementById('player-cards');
+            const cpuCardsDiv = document.getElementById('cpu-cards');
+            playerCardsDiv.innerHTML = ''; // 以前のカードをクリア
+            cpuCardsDiv.innerHTML = ''; // 以前のCPUカードをクリア
 
+            data.cards.forEach((card, index) => {
+                const cardImg = document.createElement('img');
+                cardImg.src = card.image;
+                cardImg.alt = `${card.value} of ${card.suit}`;
+                cardImg.classList.add('card');
+                cardImg.dataset.value = card.value;
+                cardImg.dataset.suit = card.suit;
+                const deckBackImg = document.createElement('img');
+                deckBackImg.src = "https://deckofcardsapi.com/static/img/back.png";
+                deckBackImg.classList.add('card');
 
-const card_type = ['&spades;', '&diams;', '&hearts;', '&clubs;'];
-let count = 0;
-//山札作成
-for (let i = 0; i < card_type.length; i++) {
+                // 2枚づつカードを配る
+                if (index < 2) {
+                    playerCardsDiv.appendChild(cardImg);
 
-    for (let j = 1; j <= 13; j++) {
-        cards[count] = new Card(card_type[i], j);
-        //   console.log(count +': type:' + card_type[i] + ' num:' + j);
-        count++;
-    }
+                    // プレイヤーの合計値を計算
+                    const cardValue = calculateCardValue(card);
+                    totalPlayer += cardValue;
+                    // 画面に表示させる
+                    document.getElementById('p_sum').textContent = totalPlayer;
+                } else {
+                    cpuCardsDiv.appendChild(deckBackImg);
 
+                    // CPUカードを保存する
+                    cpuCards.push(card);
+
+                    // CPUの合計値を計算
+                    const cardValue = calculateCardValue(card);
+                    totalCpu += cardValue;
+                    
+                }
+            });
+        });
+};
+
+// hit選択時
+function hit() {
+    fetch(`${deckApiUrl}/${deckId}/draw/?count=1`)
+        .then(response => response.json())
+        .then(data => {
+            const drawnCardsDiv = document.getElementById('player-cards');
+
+            data.cards.forEach(card => {
+                const cardImg = document.createElement('img');
+                cardImg.src = card.image;
+                cardImg.alt = `${card.value} of ${card.suit}`;
+                cardImg.classList.add('card'); 
+
+                cardImg.dataset.value = card.value;
+                
+
+                drawnCardsDiv.appendChild(cardImg);
+
+                // 合計値を計算して更新
+                const cardValue = calculateCardValue(card);
+                totalPlayer += cardValue;
+                document.getElementById('p_sum').textContent = totalPlayer;
+
+                // 合計値をチェック
+                sum_check(totalPlayer);
+
+                const target = parseInt(localStorage.getItem("target_value"), 10);
+//目標値と同じ数字になった場合hitできなくしてstandを自動的に呼ぶ
+                if(target === totalPlayer){
+                    document.getElementById("hit").disabled = true;
+                    stand();
+                }
+            });
+        });
 }
-//シャッフルする
-let i = cards.length;
-//ランダムな位置と入れ替え
-while (i) {
-    let swap_idx = Math.floor(Math.random() * i--);
-    //console.log();
-    let tmp = cards[i];
-    cards[i] = cards[swap_idx];
-    cards[swap_idx] = tmp;
-}
+
+// stand選択時
+function stand() {
+    // ヒットとスタンドのボタンを無効化する
+    document.getElementById("hit").disabled = true;
+    document.getElementById("stand").disabled = true;
+
+    // CPU の手札を表向きにする
+    const cpuCardsDiv = document.getElementById('cpu-cards');
+    cpuCardsDiv.innerHTML = ''; // 以前の裏向きカードをクリア
+
+    // 表向きのカードを追加
+    cpuCards.forEach(card => {
+        const cardImg = document.createElement('img');
+        cardImg.src = card.image;
+        cardImg.alt = `${card.value} of ${card.suit}`;
+        cardImg.classList.add('card'); 
+        cpuCardsDiv.appendChild(cardImg);
+
+        // 画面に表示させる
+        document.getElementById('c_sum').textContent = totalCpu;
+    });
 
 
-//初期手札作成
-for (picup_cnt; picup_cnt < 2; picup_cnt++) {
-    player_cards[picup_cnt] = cards[picup_cnt];
-    console.log(player_cards[picup_cnt]);
-}
+    const target = parseInt(localStorage.getItem("target_value"), 10); // 目標値を数値として取得
+    const msg = document.getElementById("msg");
+    // CPUが目標値に近づくようにカードを引く
+    function cpuDrawCard() {
+        if (totalCpu < target - 4 && totalCpu < totalPlayer) {
+            fetch(`${deckApiUrl}/${deckId}/draw/?count=1`)
+                .then(response => response.json())
+                .then(data => {
+                    const card = data.cards[0]; // 1枚引いたカード
+                    const cardImg = document.createElement('img');
+                    cardImg.src = card.image;
+                    cardImg.alt = `${card.value} of ${card.suit}`;
+                    cardImg.classList.add('card'); 
+                    cpuCardsDiv.appendChild(cardImg);
 
-viwe_and_sum();
+                    // 合計値を計算して更新
+                    const cardValue = calculateCardValue(card);
+                    totalCpu += cardValue;
+                    document.getElementById('c_sum').textContent = totalCpu;
 
-//手札、合計値表示
-function viwe_and_sum() {
-    //playerタグ取得
-    const player = document.getElementById("player");
-    const p_sum_text = document.getElementById("p_sum");
-
-    if (hit_flg === true) {
-        let qur_cnt = player_cards.length;
-        const p_show = document.createElement("p");
-
-        p_sum += cards[qur_cnt].num;
-
-        p_show.classList.add('card');
-        p_show.setAttribute('id', cards[qur_cnt]);
-        p_show.innerHTML = cards[qur_cnt].type + '<br>' + cards[qur_cnt].num;
-        p_show.style.width = '1.5em';
-        p_show.style.textAlign = 'center';
-        player.appendChild(p_show);
-
-    } else {
-        p_sum = 0;
-        count = 0;
-        for (let i = 0; i < player_cards.length; i++) {
-            const p_show = document.createElement("p");
-            let p_card = cards[count];
-
-            //合計値計算
-            console.log(p_card.num + " : " + typeof (p_card.num));
-            // let tmp = p_sum + p_card.num;
-            p_sum += p_card.num;
-
-            p_show.classList.add('card');
-            p_show.setAttribute('id', count);
-            p_show.innerHTML = p_card.type + '<br>' + p_card.num;
-            p_show.style.width = '1.5em';
-            p_show.style.textAlign = 'center';
-            player.appendChild(p_show);
-
-            count++;
+                    // 再度チェック
+                    cpuDrawCard(); // 再帰的に呼び出してカードを引き続ける
+                });
+        } else {
+            // 最終結果を表示する
+            if (totalCpu > target) {
+                msg.textContent = "You Win!";
+                msg.style.color = "red";
+            } else if (totalCpu > totalPlayer) {
+                msg.textContent = "You Lose!";
+                msg.style.color = "blue";
+            } else if (totalCpu === totalPlayer) {
+                msg.textContent = "Draw!";
+                msg.style.color = "green";
+            } else {
+                msg.textContent = "You Win!";
+                msg.style.color = "red";
+            }
         }
     }
-
-    let tmp = String(p_sum);
-    p_sum_text.textContent = tmp;
-    sum_check(p_sum);
-
+    // CPUがカードを引く処理を開始
+    cpuDrawCard();
 }
 
-
-
-
-//ヒット選択時
-function hit() {
-    picup_cnt++;
-    console.log("山札から" + picup_cnt + "枚目を引きました")
-    player_cards[picup_cnt] = cards[picup_cnt];
-    console.log(player_cards[picup_cnt]);
-    hit_flg = true;
-    viwe_and_sum();
+// カードの値を数値に変換する
+function calculateCardValue(card) {
+    const value = card.value;
+    if (value === 'ACE') {
+        // 1or11かを選ばせるロジックを書く
+        return 11;
+    } else if (['KING', 'QUEEN', 'JACK'].includes(value)) {
+        return 10;
+    } else {
+        return parseInt(value, 10); // 2から10までの数字カード
+    }
 }
 
-//スタンド選択時
-function stand() {
-
-}
-
-
-
-//合計値チェック
-function sum_check(sum){
+function sum_check(sum) {
+    const target = parseInt(localStorage.getItem("target_value"), 10); // 目標値を数値として取得
     const msg = document.getElementById("msg");
 
-    if(target - sum >= 0){
-        msg.textContent = "せーふ";
-    }else{
-        msg.textContent = "あうと";
+    if (sum <= target) {
+        // msg.textContent = "セーフ";
+    } else {
+        // バーストの文字を真ん中に大きく出せるようにする
+        msg.textContent = "バースト";
+        // ヒットとスタンドのボタンを無効化する
+        document.getElementById("hit").disabled = true;
+        document.getElementById("stand").disabled = true;
     }
-
 }
-const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
